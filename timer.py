6b19671342f3247d1ba1014260f9af623a43e030
herpydirtz <georgechan930@gmail.com>
2022-09-21 21:41:54 -0400

    Optimized timer code and fixed the issue where timer prints on seperate lines of code. Current function overwrites the previous time to keep console tidy. Added shopper and music files to be filled out with their respective functions in the next iteration. Updated Readme
    Co-authored-by: Webster Alk <wbstkr@users.noreply.github.com>

--- timer.py
+++ timer.py
@@ -5,53 +5,34 @@ def timer():
     
     seconds = 0
     
-    if "hour" in user_input or "hours" in user_input:
-        if "hour" in user_input:
+    grandfather = {
+        "hour" : 3600,
+        "minute" : 60,
+        "second" : 1
+    }
+
+    for type in grandfather.keys():
+        if type in user_input:
             try:
-                seconds += 3600 * int(user_input[user_input.index("hour") - 1])
+                seconds += grandfather[type] * int(user_input[user_input.index(type) - 1])
             except:
                 print("invalid input")
                 timer()
-        elif "hours" in user_input:
+        elif type + "s" in user_input:
             try:
-                seconds += 3600 * int(user_input[user_input.index("hours") - 1])
+                seconds += grandfather[type] * int(user_input[user_input.index(type + "s") - 1])
             except:
                 print("invalid input")
                 timer()
-    if "minute" in user_input or "minutes" in user_input:
-        if "minute" in user_input:
-            try:
-                seconds += 60 * int(user_input[user_input.index("minute") - 1])
-            except:
-                print("invalid input")
-                timer()
-        elif "minutes" in user_input:
-            try:
-                seconds += 60 * int(user_input[user_input.index("minutes") - 1])
-            except:
-                print("invalid input")
-                timer()
-    if "second" in user_input or "seconds" in user_input:
-        if "second" in user_input:
-            try:
-                seconds += int(user_input[user_input.index("second") - 1])
-            except:
-                print("invalid input")
-                timer()
-        elif "seconds" in user_input:
-            try:
-                seconds += int(user_input[user_input.index("seconds") - 1])
-            except:
-                print("invalid input")
-                timer()
-    
+
     if seconds > 0:
         for i in range(seconds, 0, -1): # i in range() is counting the items in a list that starts from seconds and ends at 0 decreasing in intervals of 1. This could be changed for example -2 would decrease 2x the rate
             m, s = divmod(i, 60)
             h, m = divmod(m, 60)
             time_left = str(h).zfill(2) + ":" + str(m).zfill(2) + ":" + str(s).zfill(2)
-            print(time_left)
+            print(time_left,end='\r')
             time.sleep(1)
+        print("00:00:00", end="\r")
         print('\n' + "The timer is complete.")
     else:
         print("inval2")
