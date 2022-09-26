a6e9753c66495092bee3ba8421de2ed1f3243d1a
herpydirtz <georgechan930@gmail.com>
2022-09-26 18:34:32 -0400

    resolving bugs

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 # imports
 from timer import timer # allows us to call timer() from timer.py
 from rng import rng
-=======
-from timer import timer  # allows us to call timer() from timer.py
->>>>>>> 6db690b65101dd4998bf2c4dd76d612c4603e4c8
 
 
 def parse_request(user_input):  # takes user_input and turns it into the valid app
@@ -29,17 +25,14 @@ def main():
 
     if user_input == "timer":  # start timer method if timer requested
         timer()
-<<<<<<< HEAD
     elif user_input == "random": # user would likely say "give me a random number from _ to _ "
         rng()
     elif user_input == "quit": # end program if user asks to quit
         quit
     else: # try again if error
-=======
     elif user_input == "quit":  # end program if user asks to quit
         exit()
     else:  # try again if error
->>>>>>> 6db690b65101dd4998bf2c4dd76d612c4603e4c8
         main()
 
 
