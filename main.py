26deb5e36d0350ca378c61c4535cdf369f2d48f5
herpydirtz <georgechan930@gmail.com>
2022-10-13 18:15:07 -0400

    Fixed bug in the main function to allow for capitalization in the input

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from apps import filer, rng, timer
 
 
 def parse_request(user_input):  # takes user_input and turns it into the valid app
-    user_input = user_input.split()
+    user_input = user_input.lower().split()
 
-    if "timer" in user_input or "countdown" in user_input:
+    if "timer" in user_input or "countdown" in user_input: #TODO: give the ability to use short phrase like "mins" and allow for the time to be inserted before the word timer ie "create a 5 minute timer"
         timer.timer(user_input)
-
-    elif "random" in user_input and "number" in user_input:
+    elif "random" in user_input and "number" in user_input:   
         rng.rng(user_input)
     elif "flip" in user_input and ("coin" in user_input or "coins" in user_input):
         rng.coin(user_input)
@@ -21,7 +20,6 @@ def parse_request(user_input):  # takes user_input and turns it into the valid a
         exit()
     else:
         main()
-
 
 def main():
     parse_request(input("What is your request?: "))
