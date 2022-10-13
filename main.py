f05febafbf4c0106e16de4217f241314657c0ea0
herpydirtz <georgechan930@gmail.com>
2022-10-13 18:17:52 -0400

    added a space

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from apps import filer, rng, timer
 def parse_request(user_input):  # takes user_input and turns it into the valid app
     user_input = user_input.lower().split()
 
-    if "timer" in user_input or "countdown" in user_input: #TODO: give the ability to use short phrase like "mins" and allow for the time to be inserted before the word timer ie "create a 5 minute timer"
+    if "timer" in user_input or "countdown" in user_input: # TODO: give the ability to use short phrase like "mins" and allow for the time to be inserted before the word timer ie "create a 5 minute timer"
         timer.timer(user_input)
     elif "random" in user_input and "number" in user_input:   
         rng.rng(user_input)
