a617f8fbb79e091472ecaa8cc75e48de662b52c2
herpydirtz <georgechan930@gmail.com>
2022-11-28 17:10:38 -0500

    Initialized a measurement and google function

---
+++ apps/measurements.py
@@ -0,0 +1,15 @@
+def measurements(user_input):
+    print("test")
+    print(user_input)
+    a = ''.join(user_input)
+    print(a)
+    if  (['to', 'liters'] in a or ['how', 'many', 'liters'] in user_input) and "cups" in a:  #current issue is that the user input is being broken down into a list of strings so they are all seperate words therfore does
+        #not recognize, need to concatenate string somehow
+        try:
+            num_of_cups = int(user_input[user_input.index("cups") - 1])
+            num_of_liters = num_of_cups * 0.236588
+            print(num_of_liters)
+            print("test")
+        except ValueError:
+            print("Could not compute amount, please repeat")
+            
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from apps import filer, rng, timer
+from apps import filer, rng, timer, measurements
 import speech_recognition as sr
 import pyttsx3
+import webbrowser as wb
 
 def parse_request():  # takes user_input and turns it into the valid app
     r = sr.Recognizer()
@@ -11,9 +12,9 @@ def parse_request():  # takes user_input and turns it into the valid app
         r.pause_threshold = 0.7
         audio = r.listen(source)
         try:
-            print("Compiling")
+            print("Recognizing")
             user_input = r.recognize_google(audio, language='en-in')
-            print("You said ", user_input)
+            print("the command is printed=", user_input)
         except Exception as e:
             print(e)
             print("Say that again sir")
@@ -27,15 +28,14 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("hello sir, I am your desktop assistant. Tell me, how may I assist you?") 
+speak("Hello this is your virtual assistant, how may I be of service?")
 
-def main():   
-
+def main():    
     while (True):
         user_input = parse_request().lower().split()
         if "timer" in user_input or "countdown" in user_input: # TODO: give the ability to use short phrase like "mins" and allow for the time to be inserted before the word timer ie "create a 5 minute timer"
             timer.timer(user_input)
-        elif "random" in user_input and "number" in user_input:   
+        elif ("random" in user_input or "pick" in user_input) and ("number" in user_input or "numbers" in user_input) in user_input:   #add "pick a number"
             rng.rng(user_input)
         elif "flip" in user_input and ("coin" in user_input or "coins" in user_input):
             rng.coin(user_input)
@@ -43,12 +43,14 @@ def main():
             rng.dice(user_input)
         elif "open" in user_input and "filer" in user_input:
             filer.filer(user_input)
-        elif "what is your name" in user_input:
-            speak("I am Vgeorge, your desktop assistant")
+        elif "open" in user_input and "google" in user_input:
+            wb.open("www.google.com")
+        elif "measurement" in user_input or "measurements" in user_input:
+            measurements.measurements(user_input)
         elif "quit" in user_input:
             exit()
         else:
-            main() 
+            main()
 
 if __name__ == "__main__":
     main()
